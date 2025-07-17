e980fa758ad0f6f2ca24ca01630f24d3e5047288
jahangeer-44 <md.jahangeer143304@gmail.com>
2025-07-17 18:02:17 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -8,35 +8,35 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
+dotenv.config();
 
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-    credentials: true
-  }
-  
-  
 
-app.use(cors(corsOptions));
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://job-nest-smoky.vercel.app", 
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions)); 
 
 const PORT = process.env.PORT || 3000;
 
-
-// api's
+// api routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
