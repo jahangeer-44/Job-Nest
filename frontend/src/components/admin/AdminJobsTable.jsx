7603248ae94adafe7aee2bb80d419dfcd7b40d83
jahangeer-44 <md.jahangeer143304@gmail.com>
2025-07-17 20:25:05 +0530

    update

--- frontend/src/components/admin/AdminJobsTable.jsx
+++ frontend/src/components/admin/AdminJobsTable.jsx
@@ -46,7 +46,6 @@ const AdminJobsTable = () => {
                                     <Popover>
                                         <PopoverTrigger><MoreHorizontal /></PopoverTrigger>
                                         <PopoverContent className="w-32">
-                                            
                                             <div onClick={()=> navigate(`/admin/jobs/${job._id}/applicants`)} className='flex items-center w-fit gap-2 cursor-pointer mt-2'>
                                                 <Eye className='w-4'/>
                                                 <span>Applicants</span>
